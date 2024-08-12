5ee5824311dca1a7090a35cd8d6c023e438b6c9a
patricialeticia <108193311+patricialeticia@users.noreply.github.com>
2024-08-12 08:26:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+ 
+consPerguntas = [
+    {
+        enunciado: Como os hábitos alimentares variam em diferentes culturas ao redor do mundo?
+        alternativas: [
+    {
+        texto: "A falta de compreensão e respeito pela cultura alimentar de uma comunidade pode levar à marginalização e à perda de tradições valiosas.",
+        afirmacao: [
+            ": Promover a educação alimentar, ensinando às gerações futuras sobre a importância de preservar  culinárias..",
+            "Promover a conscientização sobre a diversidade dos hábitos alimentares em diferentes culturas por meio de programas educacionais."
+        ]
+    },
+    {
+        texto: "Isso é maravilhoso!",
+        afirmacao: [
+            "Quis saber como usar IA no seu dia a dia.",
+            "Pensou que IA pode ajudar em tarefas da sua vida."
+        ]
+    }
+]
