759a348b323d058618eb8c771749e6f4c0899400
patricialeticia <108193311+patricialeticia@users.noreply.github.com>
2024-08-19 09:53:08 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,2 +1,16 @@
-
-            
+ export const perguntas = [
+    {
+        enunciado:"Como os hábitos alimentares variam em diferentes culturas ao redor do mundo?",
+        alternativas: [
+            {
+                texto:"A falta de compreensão e respeito pela cultura alimentar de uma comunidade pode levar à marginalização e à perda de tradições valiosas!",
+               afirmacao:"Promover a educação alimentar, ensinando às gerações futuras sobre a importância de preservar  culinárias"
+            },
+            {
+                texto:"Os hábitos alimentares variam de acordo com os recursos disponíveis, as tradições culturais, as crenças religiosas e os aspectos socioeconômicos de cada região!",
+                afirmacao:"Promover a conscientização sobre a diversidade dos hábitos alimentares em diferentes culturas por meio de programas educacionais"
+            }
+                 
+        ]
+    },
+];
