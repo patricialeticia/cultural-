2ba5111c3cb1b95edbe92475fc0c7c8eac288907
patricialeticia <108193311+patricialeticia@users.noreply.github.com>
2024-08-26 09:57:49 -0300

    Add files via upload

---
+++ js/aleatorio.js
@@ -0,0 +1,8 @@
+const nomes = ["Leticia", "Juliana", " Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
